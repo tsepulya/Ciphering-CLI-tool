a87bbc35a111f3f8adfe5e4d89af4836ee07b732
tsepulya <tsepeleva_anna@mail.ru>
2021-11-11 22:53:07 +0300

    feat: add cyphers functions

---
+++ cyphers/caesar.js
@@ -0,0 +1,28 @@
+module.exports.cipherInCaesar = (phrase, type) => {
+    let arrayNew = [];
+    for (let i = 0; i < phrase.length; i++) {
+        let pos = phrase.codePointAt(i);
+        if (type === 1) {
+            if (pos === 90) {
+                arrayNew.push('A');
+            } else if (pos === 122) {
+                arrayNew.push('a');
+            } else if ((pos >= 65 && pos < 90) || (pos >= 97 && pos < 122)) {
+                arrayNew.push(String.fromCodePoint(pos+1));
+            } else {
+                arrayNew.push(phrase[i]);
+            }
+        } if (type === 0) {
+            if (pos === 65) {
+                arrayNew.push('Z');
+            } else if (pos === 97) {
+                arrayNew.push('z');
+            } else if ((pos >= 66 && pos <= 90) || (pos >= 98 && pos <= 122)) {
+                arrayNew.push(String.fromCodePoint(pos-1));
+            } else {
+                arrayNew.push(phrase[i]);
+            }
+        }
+    }
+    return arrayNew.join('');
+}
---
+++ cyphers/rot8.js
@@ -0,0 +1,30 @@
+module.exports.cipherInRot = (phrase, type) => {
+    let arrayNew = [];
+    for (let i = 0; i < phrase.length; i++) {
+        let pos = phrase.codePointAt(i);
+        if (type === 1) {
+            let posNew = pos + 8;
+            if ((pos >= 65 && pos <= 90) || (pos >= 97 && pos <= 122)) {
+                if ((posNew > 90 && posNew <= 98 ) || (posNew > 122 && posNew <= 130)) {
+                    arrayNew.push(String.fromCodePoint(posNew - 26));
+                } else {
+                    arrayNew.push(String.fromCodePoint(posNew));
+                }
+            } else {
+                arrayNew.push(phrase[i]);
+            }
+        } else if (type === 0) {
+            let posNew = pos - 8;
+            if ((pos >= 65 && pos <= 90) || (pos >= 97 && pos <= 122)) {
+                if ((posNew >= 57 && posNew < 65) || (posNew >= 89 && posNew < 97)) {
+                    arrayNew.push(String.fromCodePoint(pos + 18));
+                } else {
+                    arrayNew.push(String.fromCodePoint(posNew));
+                }
+            } else {
+            arrayNew.push(phrase[i]);
+            }
+        }
+    }
+    return arrayNew.join('');
+}
